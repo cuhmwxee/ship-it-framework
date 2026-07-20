539a2d1f48c1b2a04a8c77167bdf12b0084f8996
Harri <hmaatta@gmail.com>
2026-07-20 20:11:42 +0300

    Removed One question

--- website/components/Framework.tsx
+++ website/components/Framework.tsx
@@ -161,7 +161,6 @@ export default function Framework() {
   return (
     <SectionContainer>
       <FrameworkBridge />
-      <FrameworkDefinition />
       <WorkflowDiagram />
     </SectionContainer>
   );
